e76b8d89f52cebdd9891080c9f4afae9ec62bb9e
Polyhistor <pouya.ataei.7@gmail.com>
2020-04-26 14:03:09 +1200

    implemented the first templating functions

--- starter/index.js
+++ starter/index.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
-const http = require('http')
-const url = require('url')
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 ///////////////////////////
 // FILES
@@ -26,51 +26,81 @@ const url = require('url')
 //             })
 //         })
 //     })
-// }) 
+// })
 
 // console.log("will read file")
 
-
-
 ///////////////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
 
+  return output;
+};
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-    const dataObj = JSON.parse(data)
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
 
+  // Overview page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
 
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
 
-const server = http.createServer((req,res)=> {
-    const pathName = req.url;
+    const output = tempOverview.replace(`{%PRODUCT_CARDS%}`, cardsHtml);
 
-    if(pathName=== "/" || pathName=== "/overview") {
-        res.end('this is the overview')
-    } else if (pathName === "/products") {
-        res.end("products")
-    } else if(pathName === "/api") {
-           res.writeHead(200, {
-               'Content-type': 'application/json'
-           })
-            res.end(data)
-        }
+    res.end(output);
 
- 
+    // Product page
+  } else if (pathName === "/products") {
+    res.end("products");
 
-    else {
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'hello-world'
-        });
-        res.end("<h1>page not found</h1>")
-    }
+    // API
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  }
 
-    
+  // Not found
+  else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>page not found</h1>");
+  }
+});
 
-})
-
-server.listen(8000, '127.0.0.1', ()=> {
-    console.log("listening to request on port 8000")
-})
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to request on port 8000");
+});
